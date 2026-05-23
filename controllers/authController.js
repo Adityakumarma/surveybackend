e03b3b88d4c5c475d1b3d9eda2a3cce0f77ea914
Adityakumarma <adityakumarma@gmail.com>
2026-05-23 11:00:51 +0530

    authcontroller fix

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,4 @@
 const Admin = require('../models/Admin');
-
 const loginAdmin = async (req, res) => {
   const { email, password } = req.body;
   try {
@@ -18,5 +17,4 @@ const loginAdmin = async (req, res) => {
     res.status(500).json({ message: 'Server error' });
   }
 };
-
-module.exports = { loginAdmin };
+module.exports = { loginAdmin }
